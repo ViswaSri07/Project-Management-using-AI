f384571bef1ef265366f7ad5d234cd8548381c77
ViswaSri7 <pasupuletiviswasri@gmail.com>
2026-04-22 20:49:54 -0500

    prisma vercel fix

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,17 @@
 import { PrismaClient } from "@prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg"
 
 declare global {
   var prisma: PrismaClient | undefined
 }
 
-export const db = globalThis.prisma || new PrismaClient()
+function createPrismaClient(): PrismaClient {
+  const adapter = new PrismaPg(process.env.DIRECT_URL as string)
+  return new PrismaClient({ adapter } as any)
+}
+
+export const db = globalThis.prisma || createPrismaClient()
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.prisma = db
 }
-
